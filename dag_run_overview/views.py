9cea0ad0410cb168eaae3d4d125103bfb5e6be13
Nick Ross <nick.ir.ross@gmail.com>
2021-05-07 13:38:42 +0100

    feat: add state filter to dag run overview page

--- dag_run_overview/views.py
+++ dag_run_overview/views.py
@@ -2,6 +2,8 @@ from airflow.models import DagBag
 from airflow.utils.db import provide_session
 from airflow.utils.state import State
 
+from flask import request
+
 from flask_admin import BaseView, expose
 
 from flask_login import login_required
@@ -12,13 +14,23 @@ class DROView(BaseView):
     @provide_session
     @login_required
     def test(self, session=None):
-
-        dags = [
-            dag
-            for dag in DagBag(include_examples=False).dags.values()
+        dags = [{
+            'dag_id': dag.dag_id,
+            'safe_dag_id': dag.safe_dag_id,
+            'schedule_interval': dag.schedule_interval,
+            'last_dag_run': dag.get_last_dagrun(),
+        } for dag in DagBag(include_examples=False).dags.values()
             if not dag.is_paused and dag.get_last_dagrun() is not None
         ]
-        return self.render("main.html", dags=dags, State=State)
+
+        state_filter = request.args.get('state')
+        if state_filter:
+            dags = filter(
+                lambda x: x['last_dag_run'].get_state() == state_filter,
+                dags
+            )
+
+        return self.render("main.html", dags=dags, State=State, filter=state_filter)
 
 
 dro_view = DROView(category="Dag Runs", name="Dag Run Overview")
